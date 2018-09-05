4dd1486d42664e416e86a8776bf251dc5c13d76c
gianna.jolibeth15@gmail.com <gianna.jolibeth15@gmail.com>
2018-09-05 14:20:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ console.log(theBeatlesPlay(musicians,instruments))
 //
 //
 var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+
+function johnLennonFacts(facts)
 var f= 0
 var lennonFacts = []
-function johnLennonFacts(facts)
-{while (f< facts.length) {lennonFacts.push(facts[f] + "!!!"); f++; }
+while (f< facts.length) {lennonFacts.push(facts[f] + "!!!"); f++; }
   return lennonFacts
 } 
 
